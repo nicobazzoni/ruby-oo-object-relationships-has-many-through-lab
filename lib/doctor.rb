946bc2d988e2fd6de9389d7d86ed0721499bb339
Nico Bazzoni <you@example.com>
2020-12-04 22:36:46 -0500

    commit tree 250a76c61e2d4ae62ea510a3000001c5a44f1cd5

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,32 @@
+require 'pry'
+class Doctor
+  @@all=[]
 
+  def self.all
+    @@all
+  end
+
+  attr_accessor :name, :appointments, :patient
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+
+  def new_appointment(date, patient)
+     appointment = Appointment.new(date, patient, self)
+     @appointments << appointment
+     appointment
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+  
+  
+  def patients  
+    appointments.map(&:patient)  
+  end
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,44 @@
+require 'pry'
+class Patient
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+   
+  
+  def new_appointment(date, doctor)
+    
+    Appointment.new(date, self, doctor)
+  end
 
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  
+    
+      def doctors 
+         appointments.map(&:doctor) 
+      # Appointment.all.collect do |appointments|
+      #    #binding.pry
+      #   appointments.doctor 
+        
+      
+    end
+  
+  
+end
+
+
+
+    
